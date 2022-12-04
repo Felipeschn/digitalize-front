0e255a3eb2b1ec300b105020606c5501a57bff7d
Felipe Schneider <demellobrl@gmail.com>
2022-12-04 17:23:55 -0300

    create duplicates view

---
+++ src/views/duplicates/CreateDuplicateModal.js
@@ -0,0 +1,143 @@
+import React, { useState, useCallback, useContext } from 'react'
+import {
+  CRow,
+  CCol,
+  CForm,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CButton,
+  CFormFloating,
+  CFormInput,
+  CFormLabel,
+  CFormTextarea,
+  CModalHeader,
+  CModalTitle,
+  CAlert,
+} from '@coreui/react'
+import api from 'src/api'
+
+import { AuthContext } from 'src/context/AuthContext'
+
+function CreateDuplicateModal({ open, handleClose, refreshList }) {
+  const { currentUserId } = useContext(AuthContext)
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [duplicateFile, setDuplicateFile] = useState('')
+  const [openCreateModal, setOpenCreateModal] = useState(open)
+  const [errMsg, setErrMsg] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log({ duplicateFile })
+
+    try {
+      await api.post(
+        `/docfile/${currentUserId}/upload`,
+        {
+          title,
+          description,
+          docType: 'duplicate',
+          file: duplicateFile,
+        },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      )
+      refreshList()
+      setOpenCreateModal(false)
+      handleClose()
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('Sem resposta do servidor')
+      } else {
+        setErrMsg('Registration Failed')
+      }
+    }
+  }
+
+  const setFormattedContent = useCallback(
+    (text) => {
+      setDescription(text.slice(0, 512))
+    },
+    [setDescription],
+  )
+
+  return (
+    <CModal
+      alignment="center"
+      scrollable
+      backdrop="static"
+      size="lg"
+      visible={openCreateModal}
+      onClose={() => {
+        setOpenCreateModal(false)
+        handleClose()
+      }}
+    >
+      <CForm onSubmit={handleSubmit}>
+        <CModalHeader>
+          <CModalTitle>Anexar</CModalTitle>
+        </CModalHeader>
+        {errMsg.length > 0 && (
+          <CAlert color="danger" style={{ textAlign: 'center' }}>
+            {errMsg}
+          </CAlert>
+        )}
+        <CModalBody>
+          <CRow xs={{ gutter: 3 }} className="mb-3">
+            <CCol md>
+              <CFormFloating>
+                <CFormInput
+                  type="text"
+                  id="title"
+                  required
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <CFormLabel htmlFor="title">Título</CFormLabel>
+              </CFormFloating>
+            </CCol>
+          </CRow>
+          <CRow xs={{ gutter: 3 }}>
+            <CCol md>
+              <CFormFloating>
+                <CFormTextarea
+                  type="text"
+                  id="description"
+                  value={description}
+                  style={{ height: '100px' }}
+                  onChange={(e) => setFormattedContent(e.target.value)}
+                />
+                <p style={{ textAlign: 'right' }} className="mb-0">
+                  {description.length}/512
+                </p>
+                <CFormLabel htmlFor="description">Descrição</CFormLabel>
+              </CFormFloating>
+            </CCol>
+          </CRow>
+          <CRow xs={{ gutter: 3 }} className="mb-3">
+            <CCol md className="mt-0">
+              <CFormLabel htmlFor="formFile">Arquivo</CFormLabel>
+              <CFormInput
+                type="file"
+                required
+                id="formFile"
+                accept="image/jpeg, image/pjpeg, image/png, application/pdf"
+                onChange={(e) => setDuplicateFile(e.target.files[0])}
+              />
+            </CCol>
+          </CRow>
+        </CModalBody>
+        <CModalFooter>
+          <CButton style={{ color: 'white' }} type="submit" color="dark">
+            Criar
+          </CButton>
+        </CModalFooter>
+      </CForm>
+    </CModal>
+  )
+}
+export default CreateDuplicateModal
---
+++ src/views/duplicates/Duplicates.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState, useContext } from 'react'
+import api from 'src/api'
+import {
+  CHeaderNav,
+  CNavItem,
+  CContainer,
+  CListGroup,
+  CListGroupItem,
+  CButton,
+  CSpinner,
+} from '@coreui/react'
+import { IoMdAdd } from 'react-icons/io'
+import { AuthContext } from 'src/context/AuthContext'
+import moment from 'moment'
+import DuplicateDetails from './DuplicateDetails'
+import CreateDuplicateModal from './CreateDuplicateModal'
+import 'moment/locale/pt-br'
+
+moment.locale('pt-br')
+
+function Duplicates() {
+  const { currentUserId } = useContext(AuthContext)
+  const [fetchData, setFetchData] = useState(false)
+  const [userDuplicates, setUserDuplicates] = useState([])
+  const [expandedRow, setExpandedRow] = useState([])
+  const [openCreateModal, setOpenCreateModal] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadData() {
+      setIsLoading(true)
+      const { data } = await api.get(`user-docfile/${currentUserId}`, {
+        params: { docType: 'duplicate' },
+      })
+      setUserDuplicates(data || [])
+      setExpandedRow([data[0].docFileId])
+    }
+    loadData()
+    setIsLoading(false)
+  }, [fetchData])
+
+  const refreshList = () => {
+    setFetchData(!fetchData)
+  }
+
+  const handleClose = () => {
+    setOpenCreateModal(false)
+  }
+
+  const getRow = (duplicate) => {
+    const expanded = !!expandedRow.find((docFileId) => docFileId === duplicate.docFileId)
+    return (
+      <>
+        <CListGroup key={duplicate.docFileId}>
+          <CListGroupItem
+            className="mb-2"
+            key={`${duplicate.docFileId}-div`}
+            component="a"
+            active={expanded}
+            style={{ width: '25rem', cursor: 'pointer' }}
+            onClick={() => setExpandedRow([duplicate.docFileId])}
+          >
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">
+                {duplicate.title.length > 23
+                  ? `${duplicate.title.substr(0, 23)} ...`
+                  : duplicate.title}
+              </h5>
+              <small>{moment(duplicate.createdAt).startOf().fromNow(true)} atrás</small>
+            </div>
+            <p className="mb-1">
+              {duplicate.description.length > 27
+                ? `${duplicate.description.substr(0, 30)} ...`
+                : `${duplicate.description.substr(0, 30)}`}
+            </p>
+          </CListGroupItem>
+        </CListGroup>
+        {expanded && <DuplicateDetails duplicate={duplicate} refreshList={refreshList} />}
+      </>
+    )
+  }
+
+  return (
+    <>
+      {isLoading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '25pc',
+          }}
+        >
+          <CSpinner />
+        </div>
+      ) : (
+        <CContainer fluid>
+          <CHeaderNav className="mb-2">
+            <CNavItem>
+              <CButton color="success" onClick={() => setOpenCreateModal(true)}>
+                <IoMdAdd className="me-1 mb-1" />
+                Anexar segunda via
+              </CButton>
+            </CNavItem>
+          </CHeaderNav>
+          {userDuplicates.map((duplicate) => getRow(duplicate))}
+          {openCreateModal && (
+            <CreateDuplicateModal
+              open={openCreateModal}
+              handleClose={handleClose}
+              refreshList={refreshList}
+            />
+          )}
+        </CContainer>
+      )}
+    </>
+  )
+}
+export default Duplicates
