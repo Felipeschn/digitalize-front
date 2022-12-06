4d49efc63609e59cfaa1efaf8abcb926f8455825
Felipe Schneider <demellobrl@gmail.com>
2022-12-05 22:10:37 -0300

    edit fdsjada

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://seal-app-vdnf4.ondigitalocean.app',
 })
+
+//'http://localhost:3333'
